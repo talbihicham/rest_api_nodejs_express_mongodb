b7cba6d6682b1e0ee05cad4084d0850d4a37fc67
hicham1999 <talbihicham99@gmail.com>
2021-04-24 17:11:40 +0000

    dotenv is done

--- app.js
+++ app.js
@@ -2,9 +2,18 @@ const express = require('express');
 const mongoose  = require('mongoose');
 const app = express();
 
+
+
+//mongoose.connect('mongodb+srv://hicham1999:' + process.env.MONGO_ATLAS_PASSWORD + '@node-rest-shop.d463t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {useNewUrlParser: true, useUnifiedTopology: true})
+
+
+
+
 //connection to the local database
 const url = 'mongodb://localhost/nodejsapi';
-mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true})
+mongoose.connect(process.env.DATABASE_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+
+//mongoose.connect(url, {useNewUrlParser:true, useUnifiedTopology: true})
 const con = mongoose.connection
 
 con.on('open', () => {
